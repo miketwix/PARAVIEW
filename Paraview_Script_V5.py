3bf3558c573505d14835f1b2f625e82e3b486f91
Miketwix PC <100409391@alumnos.uc3m.es>
2022-01-22 17:23:08 +0100

    Run Type decision tree created
    Signed-off-by: Miketwix PC <100409391@alumnos.uc3m.es>

--- Paraview_Script_V5.py
+++ Paraview_Script_V5.py
@@ -1,7 +1,8 @@
 # -------------------------------------------------------------------------------------------------------------------- #
-#   MAD FORMULA TEAM PARAVIEW CFD POST-PROCESSING SCRIPT                                                               #
-#   Version 3 (optimized Version)                                                                                    #
-#   15/08/2021                                                                                                         #
+#   MAD FORMULA TEAM PARAVIEW CFD POST-PROCESSING SCRIPT
+#   MAIN FILE
+#   Version 5                                                                                     #
+#   22/1/22                                                                                                         #
 # -------------------------------------------------------------------------------------------------------------------- #
 #   Author: Sergio Batuecas Domínguez/ Miguel Pérez Cuadrado
 #   Description: this script will be used from AWS/Windows/Ubuntu to generate all the cfd post-processing data,
@@ -17,6 +18,16 @@ import os
 import glob
 # Library to compute the time taken to run program
 import time
-# Library to do mathematical opertions on python
+# Library to do mathematical operations on python
 import numpy as np
-from Set
+# Import data from the setup file
+from Utilities.Setup import setup_info
+
+if setup_info["RunType"] == 1:
+    execfile('Single.py')
+elif setup_info["RunType"] == 2:
+    execfile('Comp.py')
+elif setup_info["RunType"] == 3:
+    execfile('FigOnly.py')
+else:
+    print("Please select a valid RunType at the Setup file")
