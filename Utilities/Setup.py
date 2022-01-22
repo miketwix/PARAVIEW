efee64747d94d9840d5aafaa8070df93a023639e
Miketwix PC <100409391@alumnos.uc3m.es>
2022-01-22 17:50:36 +0100

    output name corrected + pathlib deleted
    Signed-off-by: Miketwix PC <100409391@alumnos.uc3m.es>

--- Utilities/Setup.py
+++ Utilities/Setup.py
@@ -8,14 +8,14 @@
 #   Description: This script contains the main parameters to run the Paraview code. User shall change parameters as
 #   Desired for the different mode of use of the postprocessing run and simulation variables
 # -------------------------------------------------------------------------------------------------------------------- #
-from pathlib import Path
+
 # --------------------------------------------- RUN TYPE --------------------------------------
 # Select the type of Run that will be performed. Use [1] for single simulation analysis. Use [2] for a comparison
 # analysis and [3] for a figure only  run.
 runType = 1
 # --------------------------------------------- FOLDER CONFIGURATION --------------------------------------
-input_path = Path("/home/administrador/Escritorio/Paraview_Batch_Postproc/Case_Files")
-output_path = Path("/home/administrador/Escritorio/Paraview_Batch_Postproc/Output")
+input_path = "/home/administrador/Escritorio/Paraview_Batch_Postproc/Case_Files"
+output_path ="/home/administrador/Escritorio/Paraview_Batch_Postproc/Output"
 # ---------------------------------------------------------------------------------------------------------
-setup_file = {"inFolder": input_path, "outFolder": output_path, "RunType": runType}
+setup_info = {"inFolder": input_path, "outFolder": output_path, "RunType": runType}
 
