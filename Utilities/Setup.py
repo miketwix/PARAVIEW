13da98f8473bfc5c616922c693d0670cbae5c3c4
Miketwix PC <100409391@alumnos.uc3m.es>
2022-01-22 17:05:44 +0100

    Input/Output path creation and run mode selection
    Signed-off-by: Miketwix PC <100409391@alumnos.uc3m.es>

--- Utilities/Setup.py
+++ Utilities/Setup.py
@@ -1,1 +1,20 @@
-a = 1
+# -------------------------------------------------------------------------------------------------------------------- #
+#   MAD FORMULA TEAM PARAVIEW CFD POST-PROCESSING SCRIPT
+#   SETUP FILE
+#   Version 1                                                                                     #
+#   15/08/2021                                                                                                         #
+# -------------------------------------------------------------------------------------------------------------------- #
+#   Author: Miguel Pérez Cuadrado
+#   Description: This script contains the main parameters to run the Paraview code. User shall change parameters as
+#   Desired for the different mode of use of the postprocessing run and simulation variables
+# -------------------------------------------------------------------------------------------------------------------- #
+# --------------------------------------------- RUN TYPE --------------------------------------
+# Select the type of Run that will be performed. Use [1] for single simulation analysis. Use [2] for a comparison
+# analysis and [3] for a figure only  run.
+runType = 1
+# --------------------------------------------- FOLDER CONFIGURATION --------------------------------------
+input_path = Path("/home/administrador/Escritorio/Paraview_Batch_Postproc/Case_Files")
+output_path = Path("/home/administrador/Escritorio/Paraview_Batch_Postproc/Output")
+# ---------------------------------------------------------------------------------------------------------
+setup_file = {"inFolder": input_path, "outFolder": output_path, "RunType": runType}
+
