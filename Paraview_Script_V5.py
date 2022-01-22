8454194fc2e399e44a83db44dfb2c3c39edd348e
Miketwix PC <100409391@alumnos.uc3m.es>
2022-01-22 16:46:49 +0100

    Setup  file creation
    Signed-off-by: Miketwix PC <100409391@alumnos.uc3m.es>

--- Paraview_Script_V5.py
+++ Paraview_Script_V5.py
@@ -7,3 +7,15 @@
 #   Description: this script will be used from AWS/Windows/Ubuntu to generate all the cfd post-processing data,
 #               figures, graphs, etc. needed. Modified from traces obtained from the Paraview app.
 # -------------------------------------------------------------------------------------------------------------------- #
+
+# Import the needed libraries to work in the environment
+# Paraview library to operate program from python console
+import paraview.simple as pvs
+# Library to operate within the operating system of the user
+import os
+# Library to use global variables
+import glob
+# Library to compute the time taken to run program
+import time
+# Library to do mathematical opertions on python
+import numpy as np
