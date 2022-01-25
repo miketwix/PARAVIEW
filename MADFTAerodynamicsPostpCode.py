265785e1c351531eeeebd35dde81004018ed3061
Miketwix PC <100409391@alumnos.uc3m.es>
2022-01-25 19:02:40 +0100

    main file created
    Signed-off-by: Miketwix PC <100409391@alumnos.uc3m.es>

---
+++ MADFTAerodynamicsPostpCode.py
@@ -0,0 +1,62 @@
+# -------------------------------------------------------------------------------------------------------------------- #
+#   MAD FORMULA TEAM PARAVIEW CFD POST-PROCESSING SCRIPT
+#   MAIN FILE
+#   Version 5                                                                                     #
+#   22/1/22                                                                                                         #
+# -------------------------------------------------------------------------------------------------------------------- #
+#   Author: Sergio Batuecas Domínguez/ Miguel Pérez Cuadrado
+#   Description: this script will be used from AWS/Windows/Ubuntu to generate all the cfd post-processing data,
+#               figures, graphs, etc. needed. Modified from traces obtained from the Paraview app.
+# -------------------------------------------------------------------------------------------------------------------- #
+
+########################################################################################################################
+# ------------------------------------------------- IMPORT OF LIBRARIES NEEDED ----------------------------------------#
+########################################################################################################################
+# Import the needed libraries to work in the environment
+from paraview.simple import *
+# Library to operate within the operating system of the user
+import os
+# Library to navigate within the operating system of the user
+from pathlib import Path
+# Library to use as finder
+import glob
+# Library to compute the time taken to run program
+import time
+# Library to use .json files
+import json
+########################################################################################################################
+# ------------------------------------------------- CONFIG DATA IMPORT-------- ----------------------------------------#
+########################################################################################################################
+# Opening JSON file
+f = open("/home/administrador/PycharmProjects/Postprocessing/config.json")
+
+# returns JSON object as
+# a dictionary
+setup_info = json.load(f)
+
+# Closing file
+f.close()
+
+start_time = time.time()
+########################################################################################################################
+# ------------------------------------------------- ERASE PREVIOUS RUNS OF POSTPROC -----------------------------------#
+########################################################################################################################
+# Clean everything in output folder
+files = os.listdir(setup_info["FolderInfo"]["outFolder"])
+for f in files:
+    os. rmdir(setup_info["FolderInfo"]["outFolder"]+"/" + f)
+########################################################################################################################
+# ------------------------------------------------- Postproc run type branch selection --------------------------------#
+########################################################################################################################
+
+if setup_info["RunType"] == 1:
+    Analyze.single(setup_info)
+elif setup_info["RunType"] == 2:
+    Analyze.comparison(setup_info)
+elif setup_info["RunType"] == 3:
+    Analyze.figOnly(setup_info)
+else:
+    print("Please select a valid RunType at the Setup file and restart the run")
+
+print("The process finished successfully.")
+print("Execution time: %s seconds" % (time.time() - start_time))
