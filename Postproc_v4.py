0e381902e4ab3cabd2c0d737a077da0b93488833
miketwix <100409391@alumnos.uc3m.es>
2022-03-03 00:08:38 +0100

    organización del main

--- Postproc_v4.py
+++ Postproc_v4.py
@@ -1,11 +1,17 @@
 # -------------------------------------------------------------------------------------------------------------------- #
 #   MAD FORMULA TEAM PARAVIEW CFD POST-PROCESSING SCRIPT
 #   MAIN FILE
-#   Version 4                                                                                     #
-#   03/03/22                                                                                                         #
+#   Version 4                                                                                                          #
+#   03/03/22                                                                                                           #
 # -------------------------------------------------------------------------------------------------------------------- #
 #   Author: Sergio Batuecas / Miguel Pérez /Rafael Enríquez / Daniel Orozco
 
 #   Description: this script will be used from AWS/Windows/Ubuntu to generate all the cfd post-processing data,
 #               figures, graphs, etc. needed. Modified from traces obtained from the Paraview app.
 # -------------------------------------------------------------------------------------------------------------------- #
+
+# ------------------------------------------ Import of libraries ----------------------------------------------------- #
+import os
+import sys
+
+# ------------------------------------------ Environment configuration------------------------------------------------ #
