b0fe0575dffda6c84ebc7d088c6effb732694c90
DaniOrozco1 <100636400+DaniOrozco1@users.noreply.github.com>
2022-03-06 09:48:42 +0100

    Creo el codigo nuevo

---
+++ Cod Nuevo.py
@@ -0,0 +1,13 @@
+import os
+
+ruta = r'C:\Users\danie\Desktop\Paraview_Batch_Postproc\CaseFiles'
+
+contenido = os.listdir(ruta)
+
+for i in range(len(ruta)):
+    {
+            print(contenido[i])
+
+    }
+
+
