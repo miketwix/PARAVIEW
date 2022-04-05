8d457ab7a1bf185d6d9fe4eb67f7e82eb53c6fb1
miketwix <100409391@alumnos.uc3m.es>
2022-04-06 01:16:27 +0200

    slices review

--- Auxiliar_Functions/paraview.py
+++ Auxiliar_Functions/paraview.py
@@ -6,9 +6,10 @@ def analyze_sim (info,sim):
     # ------------------------------- Import statements and pre-analysis confiuration -------------------------------- #
     [stl_camera, slice_camera, car_camera] = car_views()
     stl_views = dict.keys(stl_camera)
+    slice_views = dict.keys(slice_camera)
     # ---------------------------------------------------------------------------------------------------------------- #
 
-    print('Curently analyzing: ' + sim.name)
+    print('Curently analyzing: ' + sim.name + ' STL')
 
     #### disable automatic camera reset on 'Show'
     paraview.simple._DisableFirstRenderCameraReset()
@@ -110,3 +111,92 @@ def analyze_sim (info,sim):
     # save screenshot
         SaveScreenshot(str(sim.outFolder)+'\\'+str(sim.name)+'_'+stl_view+'.png', renderView1, ImageResolution=info["ImageRes"])
     Delete(CarSTLDisplay)
+
+    print('Curently analyzing: ' + sim.name + ' .foam')
+
+    casefoam = SliceReader(registrationName=sim.name + 'car', FileNames=[sim.foam])
+    casefoam.MeshRegions = ['internalMesh']
+    casefoam.CellArrays = ['U', 'k', 'nut', 'omega', 'p', 'wallShearStress', 'yPlus']
+    # Properties modified on casefoam
+    casefoam.MeshRegions = ['group/symmetry', 'group/wall', 'internalMesh', 'patch/B12_TE6_B12_TE1', 'patch/B12_TE6_B12_TE147', 'patch/B12_TE6_B12_TE149', 'patch/B12_TE6_B12_TE150', 'patch/B12_TE6_B12_TE151', 'patch/B12_TE6_B12_TE152', 'patch/B12_TE6_B12_TE153', 'patch/B12_TE6_B12_TE154', 'patch/B12_TE6_B12_TE155', 'patch/B14_TE6_B14_TE37', 'patch/B15_TE6_B15_TE1004', 'patch/B15_TE6_B15_TE1005', 'patch/B15_TE6_B15_TE1050', 'patch/B15_TE6_B15_TE1052', 'patch/B15_TE6_B15_TE1056', 'patch/B15_TE6_B15_TE1057', 'patch/B15_TE6_B15_TE982', 'patch/B15_TE6_B15_TE983', 'patch/B15_TE6_B15_TE986', 'patch/B15_TE6_B15_TE987', 'patch/B15_TE6_B15_TE988', 'patch/B15_TE6_B15_TE989', 'patch/B15_TE6_B15_TE990', 'patch/B16_TE6_B16_TE1', 'patch/B16_TE6_B16_TE85', 'patch/B16_TE6_B16_TE87', 'patch/B16_TE6_B16_TE88', 'patch/B16_TE6_B16_TE89', 'patch/B16_TE6_B16_TE90', 'patch/B17_TE6_B17_TE109', 'patch/B17_TE6_B17_TE110', 'patch/B17_TE6_B17_TE112', 'patch/B18_TE6_B18_TE293', 'patch/B18_TE6_B18_TE294', 'patch/B18_TE6_B18_TE295', 'patch/B18_TE6_B18_TE296', 'patch/B18_TE6_B18_TE304', 'patch/B1_TE5_B1_TE138', 'patch/B1_TE5_B1_TE141', 'patch/B1_TE5_B1_TE142', 'patch/B1_TE5_B1_TE143', 'patch/B1_TE5_B1_TE145', 'patch/B1_TE5_B1_TE146', 'patch/B1_TE5_B1_TE2', 'patch/B20_TE6_B20_TE73', 'patch/B20_TE6_B20_TE74', 'patch/B20_TE6_B20_TE77', 'patch/B20_TE6_B20_TE79', 'patch/B21_TE6_B21_TE1', 'patch/B2_TE6_B2_TE1', 'patch/B2_TE6_B2_TE25', 'patch/B3_TE6_B3_TE1', 'patch/B3_TE6_B3_TE337', 'patch/B3_TE6_B3_TE340', 'patch/B4_TE5_B4_TE2', 'patch/B4_TE5_B4_TE242', 'patch/B4_TE5_B4_TE245', 'patch/B4_TE5_B4_TE246', 'patch/B4_TE5_B4_TE247', 'patch/B4_TE5_B4_TE248', 'patch/B4_TE5_B4_TE249', 'patch/B4_TE5_B4_TE250', 'patch/B4_TE5_B4_TE252', 'patch/B5_TE5_B5_TE86', 'patch/B5_TE5_B5_TE91', 'patch/B6_TE5_B6_TE158', 'patch/B6_TE5_B6_TE159', 'patch/B6_TE5_B6_TE161', 'patch/B6_TE5_B6_TE162', 'patch/B6_TE5_B6_TE163', 'patch/B6_TE5_B6_TE164', 'patch/B6_TE5_B6_TE165', 'patch/B6_TE5_B6_TE2', 'patch/B8_TE6_B8_TE73', 'patch/B8_TE6_B8_TE74', 'patch/B8_TE6_B8_TE77', 'patch/B9_TE6_B9_TE1']
+    # get active view
+    renderView1 = GetActiveViewOrCreate('RenderView')
+    # show data in view
+    casefoamDisplay = Show(casefoam, renderView1, 'GeometryRepresentation')
+    # get color transfer function/color map for 'p'
+    pLUT = GetColorTransferFunction('p')
+    pLUT.RescaleOnVisibilityChange = 1
+    pLUT.RGBPoints = [-42126.4296875, 0.02, 0.3813, 0.9981, -41104.121824718655, 0.02000006, 0.424267768, 0.96906969,
+                      -40081.81396193731, 0.02, 0.467233763, 0.940033043, -39059.50609915597, 0.02, 0.5102, 0.911,
+                      -38037.19823637463, 0.02000006, 0.546401494, 0.872669438, -37014.89037359328, 0.02, 0.582600362,
+                      0.83433295, -35992.582510811946, 0.02, 0.6188, 0.796, -34970.2746480306, 0.02000006, 0.652535156,
+                      0.749802434, -33947.96678524926, 0.02, 0.686267004, 0.703599538, -32925.65892246791, 0.02, 0.72,
+                      0.6574, -31903.35105968657, 0.02000006, 0.757035456, 0.603735359, -30881.043196905226, 0.02,
+                      0.794067037, 0.55006613, -29858.735334123885, 0.02, 0.8311, 0.4964, -28836.42747134254,
+                      0.021354336738172372, 0.8645368555261631, 0.4285579460761159, -27814.1196085612,
+                      0.023312914349117714, 0.897999359924484, 0.36073871343115577, -26791.811745779854,
+                      0.015976108242848862, 0.9310479513349017, 0.2925631815088092, -25769.503882998513,
+                      0.27421074700988196, 0.952562960995083, 0.15356836602739213, -24747.196020217165,
+                      0.4933546281681699, 0.9619038625309482, 0.11119493614749336, -23724.888157435827, 0.6439, 0.9773,
+                      0.0469, -22702.580294654483, 0.762401813, 0.984669591, 0.034600153, -21680.27243187314,
+                      0.880901185, 0.992033407, 0.022299877, -20657.964569091797, 0.9995285432627147,
+                      0.9995193706781492, 0.0134884641450013, -19635.656706310452, 0.999402998, 0.955036376,
+                      0.079066628, -18613.348843529107, 0.9994, 0.910666223, 0.148134024, -17591.04098074777, 0.9994,
+                      0.8663, 0.2172, -16568.733117966425, 0.999269665, 0.818035981, 0.217200652, -15546.42525518508,
+                      0.999133332, 0.769766184, 0.2172, -14524.117392403736, 0.999, 0.7215, 0.2172, -13501.809529622398,
+                      0.99913633, 0.673435546, 0.217200652, -12479.501666841057, 0.999266668, 0.625366186, 0.2172,
+                      -11457.193804059709, 0.9994, 0.5773, 0.2172, -10434.885941278364, 0.999402998, 0.521068455,
+                      0.217200652, -9412.578078497027, 0.9994, 0.464832771, 0.2172, -8390.270215715682, 0.9994, 0.4086,
+                      0.2172, -7367.96235293433, 0.9947599917687346, 0.33177297300202935, 0.2112309638520206,
+                      -6345.654490152992, 0.9867129505479589, 0.2595183410914934, 0.19012239549291934,
+                      -5323.346627371655, 0.9912458875646419, 0.14799417507952672, 0.21078892136920357,
+                      -4301.03876459031, 0.949903037, 0.116867171, 0.252900603, -3278.7309018089654, 0.903199533,
+                      0.078432949, 0.291800389, -2256.4230390276134, 0.8565, 0.04, 0.3307, -1234.1151762462832,
+                      0.798902627, 0.04333345, 0.358434298, -211.8073134649385, 0.741299424, 0.0466667, 0.386166944,
+                      810.5005493164062, 0.6837, 0.05, 0.4139]
+    pLUT.ColorSpace = 'RGB'
+    pLUT.NanColor = [1.0, 0.0, 0.0]
+    pLUT.NumberOfTableValues = 32
+    pLUT.ScalarRangeInitialized = 1.0
+
+    # trace defaults for the display properties.
+    casefoamDisplay.Representation = 'Surface'
+    casefoamDisplay.ColorArrayName = ['POINTS', 'p']
+    casefoamDisplay.LookupTable = pLUT
+    casefoamDisplay.SelectTCoordArray = 'None'
+    casefoamDisplay.SelectNormalArray = 'None'
+    casefoamDisplay.SelectTangentArray = 'None'
+    casefoamDisplay.OSPRayScaleArray = 'p'
+    casefoamDisplay.OSPRayScaleFunction = 'PiecewiseFunction'
+    casefoamDisplay.SelectOrientationVectors = 'U'
+    casefoamDisplay.ScaleFactor = 2.8000000000000003
+    casefoamDisplay.SelectScaleArray = 'p'
+    casefoamDisplay.GlyphType = 'Arrow'
+    casefoamDisplay.GlyphTableIndexArray = 'p'
+    casefoamDisplay.GaussianRadius = 0.14
+    casefoamDisplay.SetScaleArray = ['POINTS', 'p']
+    casefoamDisplay.ScaleTransferFunction = 'PiecewiseFunction'
+    casefoamDisplay.OpacityArray = ['POINTS', 'p']
+    casefoamDisplay.OpacityTransferFunction = 'PiecewiseFunction'
+    casefoamDisplay.DataAxesGrid = 'GridAxesRepresentation'
+    casefoamDisplay.PolarAxes = 'PolarAxesRepresentation'
+    casefoamDisplay.SelectInputVectors = ['POINTS', 'U']
+    casefoamDisplay.WriteLog = ''
+
+    # init the 'PiecewiseFunction' selected for 'OSPRayScaleFunction'
+    casefoamDisplay.OSPRayScaleFunction.Points = [0.08105005493164062, 0.0, 0.5, 0.0, 810.5005493164062, 1.0, 0.5, 0.0]
+
+    # init the 'PiecewiseFunction' selected for 'ScaleTransferFunction'
+    casefoamDisplay.ScaleTransferFunction.Points = [-42126.4296875, 0.0, 0.5, 0.0, 810.5005493164062, 1.0, 0.5, 0.0]
+
+    # init the 'PiecewiseFunction' selected for 'OpacityTransferFunction'
+    casefoamDisplay.OpacityTransferFunction.Points = [-42126.4296875, 0.0, 0.5, 0.0, 810.5005493164062, 1.0, 0.5, 0.0]
+    # get opacity transfer function/opacity map for 'p'
+    pPWF = GetOpacityTransferFunction('p')
+    pPWF.Points = [-42126.4296875, 0.0, 0.5, 0.0, 810.5005493164062, 1.0, 0.5, 0.0]
+    pPWF.ScalarRangeInitialized = 1
+    # create a new 'Extract Block'
+    extractBlock1 = ExtractBlock(registrationName='ExtractBlock1', Input=casefoam)
+
+    # Properties modified on extractBlock1
+    extractBlock1.Selectors = ['/Root/internalMesh']
--- Auxiliar_Functions/paraview_slice.py
+++ Auxiliar_Functions/paraview_slice.py
@@ -9,7 +9,7 @@ def slice_image(info,sim):
 
     # create a new 'OpenFOAMReader'
 
-    CarSlice = SliceReader(registrationName= sim.name + 'car', FileNames=[sim.CAR])      # preguntar a mike si cambia el sim.car
+    CarSlice = SliceReader(registrationName= sim.name + 'car', FileNames=[sim.foam])
     casefoam.MeshRegions = ['internalMesh']
     casefoam.CellArrays = ['U', 'k', 'nut', 'omega', 'p', 'wallShearStress', 'yPlus']
 
@@ -153,58 +153,6 @@ def slice_image(info,sim):
     # init the 'Plane' selected for 'HyperTreeGridSlicer'
     slice1.HyperTreeGridSlicer.Origin = [5.5, 3.5, 3.6059999256394804]
 
-    # create a new 'STL Reader'
-    mFT02_311C1402CARstl = STLReader(registrationName='MFT02_311-C1-402-CAR.stl', FileNames=['C:\\Users\\fitir\\Desktop\\Paraview_Batch_Postproc\\Case_Files\\Prueba de paraview\\MFT02_311-C1-402-CAR.stl'])
-
-    # set active source
-    SetActiveSource(mFT02_311C1402CARstl)
-
-    # show data in view
-    mFT02_311C1402CARstlDisplay = Show(mFT02_311C1402CARstl, renderView1, 'GeometryRepresentation')
-
-    # get color transfer function/color map for 'STLSolidLabeling'
-    sTLSolidLabelingLUT = GetColorTransferFunction('STLSolidLabeling')
-    sTLSolidLabelingLUT.RescaleOnVisibilityChange = 1
-    sTLSolidLabelingLUT.RGBPoints = [0.0, 0.02, 0.3813, 0.9981, 0.21428571428571427, 0.02000006, 0.424267768, 0.96906969, 0.42857142857142855, 0.02, 0.467233763, 0.940033043, 0.6428571428571428, 0.02, 0.5102, 0.911, 0.8571428571428571, 0.02000006, 0.546401494, 0.872669438, 1.0714285714285712, 0.02, 0.582600362, 0.83433295, 1.2857142857142856, 0.02, 0.6188, 0.796, 1.4999999999999996, 0.02000006, 0.652535156, 0.749802434, 1.7142857142857142, 0.02, 0.686267004, 0.703599538, 1.9285714285714284, 0.02, 0.72, 0.6574, 2.1428571428571423, 0.02000006, 0.757035456, 0.603735359, 2.357142857142857, 0.02, 0.794067037, 0.55006613, 2.571428571428571, 0.02, 0.8311, 0.4964, 2.7857142857142856, 0.021354336738172372, 0.8645368555261631, 0.4285579460761159, 3.0, 0.023312914349117714, 0.897999359924484, 0.36073871343115577, 3.2142857142857144, 0.015976108242848862, 0.9310479513349017, 0.2925631815088092, 3.4285714285714284, 0.27421074700988196, 0.952562960995083, 0.15356836602739213, 3.6428571428571432, 0.4933546281681699, 0.9619038625309482, 0.11119493614749336, 3.8571428571428568, 0.6439, 0.9773, 0.0469, 4.071428571428571, 0.762401813, 0.984669591, 0.034600153, 4.285714285714285, 0.880901185, 0.992033407, 0.022299877, 4.5, 0.9995285432627147, 0.9995193706781492, 0.0134884641450013, 4.714285714285714, 0.999402998, 0.955036376, 0.079066628, 4.928571428571429, 0.9994, 0.910666223, 0.148134024, 5.142857142857142, 0.9994, 0.8663, 0.2172, 5.357142857142857, 0.999269665, 0.818035981, 0.217200652, 5.571428571428571, 0.999133332, 0.769766184, 0.2172, 5.7857142857142865, 0.999, 0.7215, 0.2172, 6.0, 0.99913633, 0.673435546, 0.217200652, 6.2142857142857135, 0.999266668, 0.625366186, 0.2172, 6.428571428571429, 0.9994, 0.5773, 0.2172, 6.642857142857143, 0.999402998, 0.521068455, 0.217200652, 6.857142857142857, 0.9994, 0.464832771, 0.2172, 7.071428571428571, 0.9994, 0.4086, 0.2172, 7.2857142857142865, 0.9947599917687346, 0.33177297300202935, 0.2112309638520206, 7.5, 0.9867129505479589, 0.2595183410914934, 0.19012239549291934, 7.7142857142857135, 0.9912458875646419, 0.14799417507952672, 0.21078892136920357, 7.928571428571429, 0.949903037, 0.116867171, 0.252900603, 8.142857142857142, 0.903199533, 0.078432949, 0.291800389, 8.357142857142858, 0.8565, 0.04, 0.3307, 8.57142857142857, 0.798902627, 0.04333345, 0.358434298, 8.785714285714285, 0.741299424, 0.0466667, 0.386166944, 9.0, 0.6837, 0.05, 0.4139]
-    sTLSolidLabelingLUT.ColorSpace = 'RGB'
-    sTLSolidLabelingLUT.NanColor = [1.0, 0.0, 0.0]
-    sTLSolidLabelingLUT.NumberOfTableValues = 32
-    sTLSolidLabelingLUT.ScalarRangeInitialized = 1.0
-
-    # trace defaults for the display properties.
-    mFT02_311C1402CARstlDisplay.Representation = 'Surface'
-    mFT02_311C1402CARstlDisplay.ColorArrayName = ['CELLS', 'STLSolidLabeling']
-    mFT02_311C1402CARstlDisplay.LookupTable = sTLSolidLabelingLUT
-    mFT02_311C1402CARstlDisplay.SelectTCoordArray = 'None'
-    mFT02_311C1402CARstlDisplay.SelectNormalArray = 'None'
-    mFT02_311C1402CARstlDisplay.SelectTangentArray = 'None'
-    mFT02_311C1402CARstlDisplay.OSPRayScaleFunction = 'PiecewiseFunction'
-    mFT02_311C1402CARstlDisplay.SelectOrientationVectors = 'None'
-    mFT02_311C1402CARstlDisplay.ScaleFactor = 0.2915771007537842
-    mFT02_311C1402CARstlDisplay.SelectScaleArray = 'STLSolidLabeling'
-    mFT02_311C1402CARstlDisplay.GlyphType = 'Arrow'
-    mFT02_311C1402CARstlDisplay.GlyphTableIndexArray = 'STLSolidLabeling'
-    mFT02_311C1402CARstlDisplay.GaussianRadius = 0.01457885503768921
-    mFT02_311C1402CARstlDisplay.SetScaleArray = [None, '']
-    mFT02_311C1402CARstlDisplay.ScaleTransferFunction = 'PiecewiseFunction'
-    mFT02_311C1402CARstlDisplay.OpacityArray = [None, '']
-    mFT02_311C1402CARstlDisplay.OpacityTransferFunction = 'PiecewiseFunction'
-    mFT02_311C1402CARstlDisplay.DataAxesGrid = 'GridAxesRepresentation'
-    mFT02_311C1402CARstlDisplay.PolarAxes = 'PolarAxesRepresentation'
-    mFT02_311C1402CARstlDisplay.SelectInputVectors = ['CELLS', 'STLSolidLabeling']
-    mFT02_311C1402CARstlDisplay.WriteLog = ''
-
-    # init the 'PiecewiseFunction' selected for 'OSPRayScaleFunction'
-    mFT02_311C1402CARstlDisplay.OSPRayScaleFunction.Points = [0.08105005493164062, 0.0, 0.5, 0.0, 810.5005493164062, 1.0, 0.5, 0.0]
-
-    # show color bar/color legend
-    mFT02_311C1402CARstlDisplay.SetScalarBarVisibility(renderView1, True)
-
-    # get opacity transfer function/opacity map for 'STLSolidLabeling'
-    sTLSolidLabelingPWF = GetOpacityTransferFunction('STLSolidLabeling')
-    sTLSolidLabelingPWF.Points = [0.0, 0.0, 0.5, 0.0, 9.0, 1.0, 0.5, 0.0]
-    sTLSolidLabelingPWF.ScalarRangeInitialized = 1
-
     # set active source
     SetActiveSource(slice1)
 
@@ -241,14 +189,9 @@ def slice_image(info,sim):
     # toggle 3D widget visibility (only when running from the GUI)
     Hide3DWidgets(proxy=slice1.SliceType)
 
-    # show data in view
-    mFT02_311C1402CARstlDisplay = Show(mFT02_311C1402CARstl, renderView1, 'GeometryRepresentation')
 
     # reset view to fit data
     renderView1.ResetCamera(False)
-
-    # show color bar/color legend
-    mFT02_311C1402CARstlDisplay.SetScalarBarVisibility(renderView1, True)
 
     # Properties modified on slice1.SliceType
     slice1.SliceType.Origin = [5.5, 0.002919374333856495, 3.6059999256394804]
@@ -299,9 +242,6 @@ def slice_image(info,sim):
     # update the view to ensure updated data information
     renderView1.Update()
 
-    # hide data in view
-    Hide(mFT02_311C1402CARstl, renderView1)
-
     # set scalar coloring
     ColorBy(slice1Display, ('POINTS', 'U', 'Magnitude'))
 
@@ -351,12 +291,7 @@ def slice_image(info,sim):
         renderView1.CameraFocalPoint = slice_camera[slice_view]["FocalPoint"]
         renderView1.CameraViewUp = slice_camera[slice_view]["ViewUp"]
         renderView1.CameraParallelScale = slice_camera[slice_view]["ParallelScale"]
+        # save screenshot
+        SaveScreenshot(str(sim.outFolder) + '\\' + str(sim.name) + '_' + slice_view + '.png', renderView1,ImageResolution=info["ImageRes"])
 
-
-    # save screenshot
-    SaveScreenshot(str(sim.outFolder) + '\\' + str(sim.name) + '_' + slice_view + '.png', renderView1,
-                   ImageResolution=info["ImageRes"])
-
-    #================================================================
-    # addendum: following script captures some of the application
-    # state to faithfully reproduce the visualization during playback
+Delete(casefoam)
